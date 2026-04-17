389778908fe48a9c0a70d0f76155f7b8efbf0633
Arene Amezua <aamezua005@ikasle.ehu.eus>
2026-04-17 13:27:41 +0200

    #1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//arene 
 function App() {
   const [heartRate, setHeartRate] = useState(null);
   const [stressLevel, setStressLevel] = useState(null);
